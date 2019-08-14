ad6bdeabaa0b4a9b258e48bd700fa4348efb7345
Zach <robotuprising42@gmail.com>
2019-08-14 14:42:32 -0500

    Completed project 4

--- nand2tetris/nand2tetris/projects/04/fill/Fill.asm
+++ nand2tetris/nand2tetris/projects/04/fill/Fill.asm
@@ -10,5 +10,53 @@
 // When no key is pressed, the program clears the screen, i.e. writes
 // "white" in every pixel;
 // the screen should remain fully clear as long as no key is pressed.
-
-// Put your code here.
+	@8192
+	D=A
+	@total
+	M=D
+(LOOP)
+	@count
+	M=0
+	//@SCREEN
+	//D=A
+	//@addr
+	//M=D
+(INPUT)
+	@KBD
+	D=M
+	
+	@WHITE
+	D;JEQ
+	
+	
+(BLACK)
+	@count
+	D=M
+	@SCREEN
+	A=A+D
+	M=-1
+	@count
+	M=M+1
+	D=M
+	@total
+	D=D-M
+	@LOOP
+	D;JEQ
+	@INPUT
+	0;JMP
+	
+(WHITE)
+	@count
+	D=M
+	@SCREEN
+	A=A+D
+	M=0
+	@count
+	M=M+1
+	D=M
+	@total
+	D=D-M
+	@LOOP
+	D;JEQ
+	@INPUT
+	0;JMP
--- nand2tetris/nand2tetris/projects/04/mult/mult.asm
+++ nand2tetris/nand2tetris/projects/04/mult/mult.asm
@@ -6,4 +6,43 @@
 // Multiplies R0 and R1 and stores the result in R2.
 // (R0, R1, R2 refer to RAM[0], RAM[1], and RAM[2], respectively.)
 
-// Put your code here.
+	@i
+	M=1
+	@sum
+	M=0
+(LOOP)
+	@R0
+	D=M
+	@STOP
+	D;JEQ
+	
+	@R1
+	D=M
+	@STOP
+	D;JEQ
+	
+	@R1
+	D=M
+	@i
+	D=D-M
+	@STOP
+	D;JLT
+	
+	@R0
+	D=M
+	@sum
+	M=M+D
+	@i
+	M=M+1
+	@LOOP
+	0;JMP
+
+(STOP)
+	@sum
+	D=M
+	@R2
+	M=D
+
+(END)
+	@END
+	0;JMP
